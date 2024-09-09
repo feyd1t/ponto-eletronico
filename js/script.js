8123a3e67c6f22842780376a702db13822365eee
feyd1t <guilherme.feydit@sempreceub.com>
2024-09-09 10:07:19 -0300

    aula 90/09

--- js/script.js
+++ js/script.js
@@ -1,12 +1,3 @@
-
-navigator.geolocation.getCurrentPosition((position) => {
-    console.log(position);
-    console.log(position.coords.latitude);
-    console.log(position.coords.longitude);
-});
-  
-
-
 const diaSemana = document.getElementById("dia-semana");
 const dataAtual = document.getElementById("data-atual");
 const horaAtual = document.getElementById("hora-atual");
@@ -14,75 +5,116 @@ const btnRegistrarPonto = document.getElementById("btn-registrar-ponto");
 
 btnRegistrarPonto.addEventListener("click", register);
 
-diaSemana.textContent = getWeekDay(); 
+diaSemana.textContent = getWeekDay();
 dataAtual.textContent = getCurrentDate();
+
 
 const dialogPonto = document.getElementById("dialog-ponto");
 
 const dialogData = document.getElementById("dialog-data");
 dialogData.textContent = getCurrentDate();
 
-
 const dialogHora = document.getElementById("dialog-hora");
 dialogHora.textContent = getCurrentTime();
 
 
-const btnDialogFechar = document.getElementById("dialog-fechar");
+const btnDialogEntrada = document.getElementById("btn-dialog-entrada");
+btnDialogEntrada.addEventListener("click", () => {
+    
+    let currentDate = getCurrentDate();
+    let currentTime = getCurrentTime();
+    let userLocation = getUserLocation();
 
-btnDialogFechar.addEventListener("click",() => {
+    ponto = {
+        "date": currentDate,
+        "time": currentTime,
+        "location": userLocation,
+        "id": 1,
+        "type": "entrada"
+    }
+
+    console.log(ponto);
+});
+
+
+const btnDialogSaida = document.getElementById("btn-dialog-saida");
+btnDialogSaida.addEventListener("click", () => {
+    console.log(getObjectRegiste("entrada"));
+
+});
+
+
+
+function getObjectRegiste(registerType){
+    
+    let currentDate = getCurrentDate();
+    let currentTime = getCurrentTime();
+    let userLocation = userLocation();
+    
+    ponto = {
+        "date": currentDate,
+        "time": currentTime,
+        "location": userLocation,
+        "id": 1,
+        "type": "saida"
+
+    }
+    
+    return ponto;
+}
+
+const btnDialogFechar = document.getElementById("dialog-fechar");
+btnDialogFechar.addEventListener("click", () => {
     dialogPonto.close();
 })
 
-function register(){
-   dialogPonto.showModal();
-    //dialog é o elemento dialogPonto
-    // e o método que abre o dialog em estilo modal é o showDialog()
+
+
+
+function getUserLocation() {
+    navigator.geolocation.getCurrentPosition((position) => {   
+        let userLocation = {
+            "lat": position.coords.latitude,
+            "long": position.coords.longitude
+        }
+        locationUser = userLocation;
+        console.log(locationUser);
+    });
 }
 
 
-
-function updateContentHour(){
-   horaAtual.textContent = getCurrentTime();
-    
+function register() {
+    dialogPonto.showModal();
 }
-const date = new Date();
 
-// Retorna a hora atual no formato hh:mm:ss
+
+function updateContentHour() {
+    horaAtual.textContent = getCurrentTime();
+}
+
+// Retorna a hora atual (hora/minuto/segundo)
 function getCurrentTime() {
     const date = new Date();
-    return date.getHours().toString().padStart(2, '0') + ":" + 
-           date.getMinutes().toString().padStart(2, '0') + ":" + 
-           date.getSeconds().toString().padStart(2, '0');
+    return String(date.getHours()).padStart(2, '0') + ":" + String(date.getMinutes()).padStart(2, '0') + ":" + String(date.getSeconds()).padStart(2, '0');
 }
 
 // Retorna a data atual no padrão dd/mm/aaaa
 function getCurrentDate() {
-    const date = new Date();
-    const day = String(date.getDate()).padStart(2, '0');
-    const month = String(date.getMonth() + 1).padStart(2, '0');
-    const year = date.getFullYear();
-    
-    return `${day}/${month}/${year}`;
+    const date = new Date(); 
+    let mes = date.getMonth() + 1;
+    return String(date.getDate()).padStart(2, '0') + "/" + String(mes).padStart(2, '0') + "/" +  String(date.getFullYear()).padStart(2, '0');
 }
 
-function getWeekDay(){
+function getWeekDay() {
     const date = new Date()
     const day = date.getDay()
-    const daynames = ["Domingo", "Segunda-Feira", "Terça-Feira", "Quarta-Feira", "Quinta-Feira,", "Sexta-Feira", "Sábado"];
-    return daynames [day];
-    
+    const daynames = ["Domingo", "Segunda-feira", "Terça-feira", "Quarta-feira", "Quinta-feira", "Sexta-feira", "Sábado"];
+    return daynames[day]
 }
+
 updateContentHour();
 setInterval(updateContentHour, 1000);
 
-// Verifica e obtém o timestamp atual em milissegundos
-var timeInMs = Date.now();
-if (!Date.now) {
-    Date.now = function now() {
-        return new Date().getTime();
-    };
-}
-
-console.log("Hora atual:", getCurrentTime());
-console.log("Data atual:", getCurrentDate());
-console.log("Timestamp atual em ms:", timeInMs);
+console.log(getCurrentTime());
+console.log(getCurrentDate());
+console.log(getWeekDay());
